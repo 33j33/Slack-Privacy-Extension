6c6d8a9d195b6feeabc51e7378d4b6cb92952027
Zaif Senpai <escbutt@gmail.com>
2025-04-26 00:40:41 +0500

    refactor: migrate extension to TypeScript and restructure codebase

---
+++ types.d.ts
@@ -0,0 +1,9 @@
+declare type ExtensionSettings = {
+  enabled: boolean;
+  blurMedia: boolean;
+  blurLinkPreviews: boolean;
+  blurPublicChannels: boolean;
+  blurHuddleMessages: boolean;
+  blurReactionsBar: boolean;
+  hoverTimeout: number;
+}
